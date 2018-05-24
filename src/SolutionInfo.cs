97c25e5a74428798219c8584ec58f78d8b041d2a
Michał Kowal <michalkowal@users.noreply.github.com>
2018-05-24 15:12:29 +0200

    Create empty solution for Cake.Addin (#10)
    * [#2] Create VisualStudio solution with empty projects
    
    * [#2] Add shared solution items (StyleCop settings and rules; rulesets; project info)
    
    * [#2] Set Tests project settings

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct("Cake.MkDocs")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Cake Contributions 2016 - Present")]
